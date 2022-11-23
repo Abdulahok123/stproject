6af1adc23f8aaec570edc6d51f81c1126ddbef7e
Abdulahok123 <55195406+Abdulahok123@users.noreply.github.com>
2022-11-23 18:00:45 +0500

    Update customSteps.js

--- customSteps.js
+++ customSteps.js
@@ -1,5 +1,5 @@
 const{When, Then, Given, After, Before, setDefaultTimeout}=require('@cucumber/cucumber');
-const {expect}= require('chai');
+const {expect, assert}= require('chai');
 const { By } = require('selenium-webdriver');
 let sum=0;
 const webdriver = require('selenium-webdriver');
@@ -112,3 +112,159 @@ When('I press the remove option', async ()=> {
             let text = await driver.findElement(By.css('.MuiGrid-item')).getText()
             console.log('cart after removing item\n'+text);
           });
+Given('I am at products page', async () => {
+  await driver.get('http://localhost:3000/s/1')
+  await driver
+    .manage()
+    .window()
+    .maximize()
+  await driver.sleep(10000)
+})
+When('I select a particular color', async () => {
+  await driver
+    .findElement(
+      By.css(
+        '#__next > main > div > div.makeStyles-root-119 > div.PrivateHiddenCss-xsDown-31 > div > div > div.RSFFilter-facetGroups-174 > div:nth-child(1) > div.MuiCollapse-container.MuiCollapse-entered > div > div > div > div > div > div:nth-child(3) > button'
+      )
+    )
+    .click()
+  await driver.sleep(12000)
+})
+
+Then('It shall show me the products of that color only', async () => {
+  let class1 = await driver
+    .findElement(
+      By.css(
+        '#__next > main > div > div.makeStyles-root-119 > div.PrivateHiddenCss-xsDown-31 > div > div > div.RSFFilter-facetGroups-174 > div:nth-child(1) > div.MuiCollapse-container.MuiCollapse-entered > div > div > div > div > div > div:nth-child(3) > button > div.RSFImage-root-143.RSFImage-contain-146.RSFImage-fill-147 > img'
+      )
+    )
+    .getAttribute('alt')
+
+  let class2 = await driver
+    .findElement(
+      By.css(
+        '#item-0 > div > div.makeStyles-info-142 > div.RSFProductOptionSelector-root-148 > div:nth-child(3) > div > button > div.RSFSwatchProductOption-disabled-161.RSFImage-root-143.RSFImage-contain-146.RSFImage-fill-147 > img'
+      )
+    )
+    .getAttribute('alt')
+  assert.equal(class1, class2)
+  await driver.findElement(By.css('#item-0 > div > div:nth-child(1) > a > div > img')).click()
+  await driver.sleep(15000)
+  await driver
+    .findElement(
+      By.css(
+        '#__next > main > div > form > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-4 > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-sm-6.MuiGrid-grid-md-7 > div > div:nth-child(1) > div.RSFProductOptionSelector-root-148 > div:nth-child(3) > div > button > div.RSFSwatchProductOption-strikeThrough-162.RSFSwatchProductOption-defaultStrikeThrough-163'
+      )
+    )
+    .isDisplayed()
+})
+
+When('I select a specific size', async () => {
+  await driver
+    .findElement(
+      By.css(
+        '#__next > main > div > div.makeStyles-root-87 > div.PrivateHiddenCss-xsDown-31 > div > div > div.RSFFilter-facetGroups-165 > div:nth-child(2) > div.MuiCollapse-container.MuiCollapse-entered > div > div > div > div > div > button:nth-child(4)'
+      )
+    )
+    .click()
+  await driver.sleep(12000)
+})
+
+Then('It shall show me products of that size only', async () => {
+  await driver.findElement(By.css('#item-0 > div > div:nth-child(1) > a > div > img')).click()
+  await driver.sleep(15000)
+  await driver
+    .findElement(
+      By.css(
+        '#__next > main > div > form > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-4 > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-sm-6.MuiGrid-grid-md-7 > div > div:nth-child(2) > div.RSFProductOptionSelector-root-149 > div:nth-child(4) > div'
+      )
+    )
+    .isDisplayed()
+})
+
+When('I check old checkbox', async () => {
+  await driver
+    .findElement(
+      By.css(
+        '#__next > main > div > div.makeStyles-root-87 > div.PrivateHiddenCss-xsDown-31 > div > div > div.RSFFilter-facetGroups-165 > div:nth-child(3) > div.MuiCollapse-container.MuiCollapse-entered > div > div > div > div > div > label:nth-child(2) > span.MuiButtonBase-root.MuiIconButton-root.PrivateSwitchBase-root-192.MuiCheckbox-root.MuiCheckbox-colorPrimary.MuiIconButton-colorPrimary > span.MuiIconButton-label > input'
+      )
+    )
+    .click()
+  await driver.sleep(12000)
+})
+
+Then('It shall show me old products only', async () => {
+  let num1 = parseInt(
+    await driver
+      .findElement(
+        By.css(
+          '#__next > main > div > div.makeStyles-root-87 > div.PrivateHiddenCss-xsDown-31 > div > div > div.RSFFilter-facetGroups-165 > div:nth-child(3) > div.MuiCollapse-container.MuiCollapse-entered > div > div > div > div > div > label:nth-child(2) > span.MuiTypography-root.MuiFormControlLabel-label.MuiTypography-body1 > div > span.MuiTypography-root.RSFCheckboxFilterGroup-matches-190.MuiTypography-caption'
+        )
+      )
+      .getText()
+  )
+
+  let num2 = parseInt(
+    await driver
+      .findElement(
+        By.css(
+          '#__next > main > div > div.makeStyles-root-87 > div.MuiGrid-root.MuiGrid-container > div:nth-child(6) > span > span'
+        )
+      )
+      .getText()
+  )
+  assert.notEqual(num1, num2)
+})
+
+When('I press the Cart Icon', async () => {
+  await driver
+    .findElement(
+      By.css(
+        '#__next > header > div > div > a.MuiButtonBase-root.MuiIconButton-root.RSFCartButton-link-24.MuiIconButton-colorInherit'
+      )
+    )
+    .click()
+  await driver.sleep(12000)
+})
+
+Then('I am redirected to the cart’s page', async () => {
+  await driver.findElement(By.css('#__next > main > div > div:nth-child(1) > h6')).isDisplayed()
+})
+
+Given('I add a product to cart', async () => {
+  await driver.get('http://localhost:3000/s/1')
+  await driver.sleep(12000)
+  await driver.findElement(By.id('item-0')).click()
+  await driver.sleep(12000)
+  await driver.findElement(By.css('.makeStyles-noShadow-207')).click()
+  await driver.sleep(12000)
+})
+
+When('I am on cart page', async () => {
+  await driver.get('http://localhost:3000/cart')
+  await driver
+    .manage()
+    .window()
+    .maximize()
+  await driver.sleep(12000)
+})
+
+Then(
+  'I am shown the specific product\\(s) price\\(s) and the Total cost on the cart page',
+  async () => {
+    await driver
+      .findElement(
+        By.css(
+          '#__next > main > div > div:nth-child(2) > div > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-sm-8 > div:nth-child(1) > div > div:nth-child(2) > p'
+        )
+      )
+      .isDisplayed()
+    await driver
+      .findElement(
+        By.css(
+          '#__next > main > div > div:nth-child(2) > div > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-sm-4 > div > div.makeStyles-root-119 > h6'
+        )
+      )
+      .isDisplayed()
+  }
+)
